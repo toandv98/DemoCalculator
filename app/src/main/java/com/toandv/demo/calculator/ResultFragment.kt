c5ba24be528f668c3a617af5b516627764d112b3
Đặng Văn Toản <toan98.k10@gmail.com>
2020-06-17 20:10:28 +0700

    fix result

--- app/src/main/java/com/toandv/demo/calculator/ResultFragment.kt
+++ app/src/main/java/com/toandv/demo/calculator/ResultFragment.kt
@@ -12,7 +12,7 @@ import java.math.MathContext
 
 class ResultFragment : Fragment(), FragmentResultListener {
 
-    private var input = ""
+    private var inputText = ""
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,49 +25,56 @@ class ResultFragment : Fragment(), FragmentResultListener {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        input = savedInstanceState?.getString(BUNDLE_INPUT_STATE) ?: ""
+        inputText = savedInstanceState?.getString(BUNDLE_INPUT_STATE) ?: ""
         parentFragmentManager.setFragmentResultListener(REQUEST_KEYBOARD, this, this)
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        outState.putString(BUNDLE_INPUT_STATE, input)
+        outState.putString(BUNDLE_INPUT_STATE, inputText)
     }
 
     override fun onFragmentResult(requestKey: String, result: Bundle) {
 
-        when (val key = result.getCharSequence(BUNDLE_INPUT)) {
+        inputText = when (val key = result.getCharSequence(BUNDLE_INPUT)) {
             getString(R.string.key_equal) -> {
 
-                val str = edtResult.text.toString()
-                val err = getString(R.string.err_expression_invalid)
+                val resultValue = edtResult.text.toString()
+                val error = getString(R.string.err_expression_invalid)
 
-                if (str.isBlank() || str == err) {
-                    edtResult.setText(err)
-                    return
+                when {
+                    resultValue.isBlank() || resultValue == error -> {
+                        edtResult.setText(error)
+                    }
+                    else -> {
+                        inputText = resultValue
+                        edtInput.setText(resultValue)
+                        edtInput.setSelection(inputText.length)
+                    }
                 }
+                return
             }
-            getString(R.string.key_backspace) -> input = input.dropLast(1)
-            else -> input = input.plus(key)
+            getString(R.string.key_backspace) -> inputText.dropLast(1)
+            else -> inputText.plus(key)
         }
 
         calculator()
-        edtInput.setText(input)
-        edtInput.setSelection(input.length)
+        edtInput.setText(inputText)
+        edtInput.setSelection(inputText.length)
     }
 
     private fun calculator() {
 
-        input = input.replace("([+×÷-])\\1+".toRegex(), "$1")
+        inputText = inputText.replace("([+×÷-])\\1+".toRegex(), "$1")
 
         val expression = Expression(
-            input.replace(getString(R.string.key_multiplied), "*")
+            inputText.replace(getString(R.string.key_multiplied), "*")
                 .replace(getString(R.string.key_divided), "/")
                 .replace(getString(R.string.key_square_root), "sqrt")
         )
 
         when {
-            input.isBlank() -> edtResult.setText(getString(R.string.default_result))
+            inputText.isBlank() -> edtResult.setText(getString(R.string.default_result))
             expression.checkSyntax() -> edtResult.setText(
                 expression.calculate().toBigDecimal(MathContext.DECIMAL64).toPlainString()
             )
